9dc94941c24367d21b9631436f1a6088dd31ec84
Simone Michelon <simomike2015@gmail.com>
2025-01-16 01:07:22 +0100

    Time: 0 ms (100%), Space: 44 MB (7.19%) - LeetHub

--- 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.java
@@ -11,27 +11,20 @@ class Solution {
                 max = i;
             }
         }
-
-        if(min == max || max % min == 0){
-            return min;
-        }
-
-        
         int half = min / 2;
 
-        if(half <= 1){
+        if(min == max || max % min == 0 ){
+            return min;
+        } else if(half <= 1){
             return 1;
         }
-        System.out.println(min + " | " + max + " HALF : " + half);
-
+        
         for(int i = half; i >=1; i--){
-            System.out.println(i);
             if((min % i == 0) && (max % i == 0)){
                 return i;
             }
         }
 
         return 0;
-        
     }
 }
